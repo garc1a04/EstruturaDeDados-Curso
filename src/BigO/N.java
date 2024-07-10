d8c43759ad2352fd98c3a437451ff8862134925f
Guilherme Garcia Monteiro <guigarciamonteiro@gmail.com>
2024-07-09 22:31:49 -0300

    BigO

---
+++ src/BigO/N.java
@@ -0,0 +1,16 @@
+package BigO;
+
+public class N {
+	public static void printItems(int n) {
+        for(int i = 0; i < n;i++){
+            System.out.println(i);
+        }
+    }
+
+
+
+    // DO NOT CHANGE THE MAIN METHOD BELOW
+    public static void main(String[] args) {
+        printItems(10);
+    }
+}
